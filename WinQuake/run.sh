2b3021a3705e04c72ac44284015aabc7680dd9ac
m <m@bitsnbites.eu>
2021-07-19 19:18:43 +0200

    run.sh: Make profiling optional

--- WinQuake/run.sh
+++ WinQuake/run.sh
@@ -1,3 +1,11 @@
-mrisc32-elf-readelf -sW out/mc1quake | grep FUNC | awk '{print $2,$8}' > /tmp/mc1quake-symbols
-mr32sim -g -ga 1073744608 -gp 1073743556 -gd 8 -gw 320 -gh 180 -P /tmp/mc1quake-symbols "$@" out/mc1quake.bin
+#!/bin/bash
+
+PROFILE_ARGS=""
+if [ "$1" == "--profile" ] ; then
+    shift 1
+    mrisc32-elf-readelf -sW out/mc1quake | grep FUNC | awk '{print $2,$8}' > /tmp/mc1quake-symbols
+    PROFILE_ARGS="-P /tmp/mc1quake-symbols"
+fi
+
+mr32sim -g -ga 1073744608 -gp 1073743556 -gd 8 -gw 320 -gh 180 ${PROFILE_ARGS} "$@" out/mc1quake.bin
 
